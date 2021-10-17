f6475701b179595179539ede10c22cf0375e7421
Ananya Dash <68226690+ananyadash@users.noreply.github.com>
2021-10-17 19:03:30 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -7,3 +7,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('JobPortal.urls'))
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('',home,name='home'),
+    path('login/',loginUser,name='login'),
+    path('logout/',logoutUser,name='logout'),
+    path('register/',registerUser,name='register'),
+    path('apply/',applyPage,name='apply'),
+]
